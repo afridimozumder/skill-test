13b3cf5a92290d97861aae98e074ef1720f2017c
Afridi Mozumder <49424469+afridimozumder@users.noreply.github.com>
2022-01-01 19:33:56 +0600

    Add Customer UI Added

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,19 +1,7 @@
-import './mystyle.css'
+import AddCustomer from "./AddCustomer";
 function App() {
   return (
-    <>
-    <form>
-      <input type="text" id="fname" name="code" placeholder="Code"></input>
-      <input type="text" id="fname" name="name" placeholder="Full Name"></input>
-      <input type="text" id="fname" name="age" placeholder="Age"></input>
-      <select id="country" name="country">
-      <option value="Bashundhara">Bashundhara</option>
-      <option value="Gulshan">Gulshan</option>
-      <option value="Banani">Banani</option>
-      </select>
-      <button class="button button4">ADD</button>
-    </form>
-    </>
+    <AddCustomer />
   );
   }
 
